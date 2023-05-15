1e7421ed5a62022c16222791d1915b5adaf62ff5
yusver1 <yusverortega@gmail.com>
2023-05-14 21:24:56 -0300

    agregando portafolio

--- src/main/java/com/portfolio/portfolioback/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/portfolioback/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"portafolioback-production-9df7.up.railway.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
