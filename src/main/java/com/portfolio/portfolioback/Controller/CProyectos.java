9f46c43fb1ff1b3b79be9863295fe8a05ca66d19
yusver1 <yusverortega@gmail.com>
2023-05-14 21:43:15 -0300

    https

--- src/main/java/com/portfolio/portfolioback/Controller/CProyectos.java
+++ src/main/java/com/portfolio/portfolioback/Controller/CProyectos.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyectos")
-@CrossOrigin(origins = {"portafolioback-production-9df7.up.railway.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portafolioback-production-9df7.up.railway.app","http://localhost:4200"})
 public class CProyectos {
     
     @Autowired
